bd8e6a2002cf823d0e7decb4ee834fa82d8b7b89
Thiago Andrade <thiagoh@gmail.com>
2015-07-14 19:04:15 -0300

    script improvements

--- enconv.sh
+++ enconv.sh
@@ -4,6 +4,9 @@ directorypresent=0
 extensionpresent=0
 frompresent=0
 topresent=0
+recursive=0
+quiet=0
+summary=1
 
 while [[ $# > 0 ]]
 do
@@ -29,6 +32,15 @@ do
 	    to="$2"
 	    topresent=1
 	    shift # past argument
+	    ;;
+	    -r|--recursive)
+	    recursive=1
+	    ;;
+	    -q|--quiet)
+	    quiet=1
+	    ;;
+	    --omit-summary)
+	    summary=0
 	    ;;
 	    *)
 	    # unknown option
@@ -63,18 +75,34 @@ if [[ ${extension:0:1} != "." ]]; then
 	extension="."$extension
 fi
 
-echo "Converting all files that ends with \"$extension\" from $from to $to"
+recursivecmd="-maxdepth 1"
 
-for curfile in $(find "$directory" -name "*$extension")
+if [[ $recursive == 1 ]]; then
+	recursivecmd=""
+fi
+
+if [[ $summary == 1 ]]; then
+	echo "Converting files that ends with \"$extension\" from $from to $to "
+fi
+
+filescount=0
+filesconvcount=0
+for curfile in $(find "$directory" $recursivecmd -name "*$extension")
 do 
 	enc="$(file -b --mime-encoding $curfile)"
-
+	filescount=$((filescount+1))
 	if [ "$enc" = "$from" ]; then
-
 		# Documentation
 		# http://mindspill.net/computing/linux-notes/determine-and-change-file-character-encoding/
 		iconv -f $from -t $to -o $curfile.tmp $curfile
 		mv $curfile.tmp $curfile
-		echo "The file $curfile was converted from $enc to $to successfuly"
+		filesconvcount=$((filesconvcount+1))
+		if [[ $quiet == 0 ]]; then
+			echo "The file $curfile was converted from $enc to $to successfuly"
+		fi
 	fi
 done
+
+if [[ $summary == 1 ]]; then
+	echo "$filescount files were found and $filesconvcount were converted"
+fi
